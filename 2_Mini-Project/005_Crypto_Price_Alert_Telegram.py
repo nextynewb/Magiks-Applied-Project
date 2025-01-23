0f0f5e1ed059a38bb99c89b309cdc50d069a0213
megatdanial <46470981+megatdanial@users.noreply.github.com>
2025-01-23 13:51:48 +0000

    005 Exercise by Megat

--- 2_Mini-Project/005_Crypto_Price_Alert_Telegram.py
+++ 2_Mini-Project/005_Crypto_Price_Alert_Telegram.py
@@ -48,12 +48,46 @@ f'https://api.mybitx.com/api/1/ticker?pair={symbol}MYR'
 """
 
 import requests
+import time
 
 TELEGRAM_API = '7500850112:AAHZGnF83amv0bsBC0gSLuBTjyuteK7faIg'
-CHAT_ID = 'YOUR_CHAT_ID'
+CHAT_ID = '540041214'
+
+cryptocurrencies = {
+    "XRP": {
+        "lower_bound": 25000,
+        "upper_bound": 30000,
+    },
+    "ETH": {
+        "lower_bound": 1500,
+        "upper_bound": 2000,
+    },
+    "SOL": {
+        "lower_bound": 1000,
+        "upper_bound": 2000,
+    }
+}
 
 def send_message(text):
     url = f'https://api.telegram.org/bot{TELEGRAM_API}/sendMessage'
     params = {'chat_id': CHAT_ID, 'text': text}
     requests.post(url, params=params)
 
+def crypto(symbol):
+    url = f'https://api.mybitx.com/api/1/ticker?pair={symbol}MYR'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return float(response.json()['last_trade'])
+
+
+while True:
+    time.sleep(1)
+    for key, value in cryptocurrencies.items():
+        price = crypto(key)
+        print(price)
+        print(value['lower_bound'])
+        print(value['upper_bound'])
+        if price > value['lower_bound'] and price < value['upper_bound']:
+            send_message(key + ' buy now!')
+        
+        
