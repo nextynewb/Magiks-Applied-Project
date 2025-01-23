12b2ecd1de115ed8e36720c9d4f2f372e203a708
mdluthfi1 <162030229+mdluthfi1@users.noreply.github.com>
2025-01-23 14:46:13 +0000

    wip

--- 2_Mini-Project/006_Listing_Mudah.py
+++ 2_Mini-Project/006_Listing_Mudah.py
@@ -40,6 +40,75 @@ Macbook Air: https://search.mudah.my/v1/search?q=macbook%20air
 
 
 import requests
+import json
+
+api_url = f'https://search.mudah.my/v1/search?q={product_name}'
+alerts = []
+
+
+def get_listing_mudah(api_url):
+    response = requests.get(api_url)
+    if response.status_code == 200:
+        data = response.json()
+        alerts.extend(data)
+    else:
+        print("Failed to get data from the API")
+
+def get_latest_listing(alerts):
+    for alert in alerts:
+        return()
+    
+while True:
+    product_name = "Macbook"
+    response = requests.get(f'https://search.mudah.my/v1/search?q={product_name}')
+    print(response.json())
+
+
+import requests
+import json
+import time
+
+TELEGRAM_API = '7500850112:AAHZGnF83amv0bsBC0gSLuBTjyuteK7faIg'
+CHAT_ID = '677286250'
+
+def send_message(text):
+    url = f'https://api.telegram.org/bot{TELEGRAM_API}/sendMessage'
+    params = {'chat_id': CHAT_ID, 'text': text}
+    requests.post(url, params=params)
+
+alert_products = [
+    "Macbook Pro", "PS5"
+]
+
+
+all_items = []
+
+for item in alert_products:
+    product_name = item
+    response = requests.get(f'https://search.mudah.my/v1/search?q={product_name}')
+    data = response.json()['data']
+
+    for x in data:
+        url = x['attributes']['adview_url']
+        all_items.append(url)
+
+while True:
+    for item in alert_products:
+        product_name = item
+        response = requests.get(f'https://search.mudah.my/v1/search?q={product_name}')
+        data = response.json()['data']
+
+        for x in data:
+            url = x['attributes']['adview_url']
+            if url in all_items:
+                continue
+            else:
+                print(url)
+                print('-----')
+        
+    time.sleep(10)
+
+
 
 TELEGRAM_API = '7500850112:AAHZGnF83amv0bsBC0gSLuBTjyuteK7faIg'
 CHAT_ID = 'YOUR_CHAT_ID'
